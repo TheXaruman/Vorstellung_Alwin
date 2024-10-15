2a63adc14e7cd333cf356b8951f13dd83f7246b1
TheXaruman <141373700+TheXaruman@users.noreply.github.com>
2024-10-15 15:24:49 +0200

    Add files via upload

---
+++ Aimo_Woche_10/scripts/script.js
@@ -0,0 +1,4 @@
+// Toggle functionality for the hamburger menu (just a placeholder functionality for now)
+document.getElementById('menuIcon').addEventListener('click', function() {
+    alert('Menu Clicked');
+});
