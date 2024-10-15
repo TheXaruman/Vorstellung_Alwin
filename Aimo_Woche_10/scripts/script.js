46a4ec7d8e3980cf5e5d862e3919657b286f9765
TheXaruman <141373700+TheXaruman@users.noreply.github.com>
2024-10-15 15:28:57 +0200

    Delete Aimo_Woche_10 directory

--- Aimo_Woche_10/scripts/script.js
+++
@@ -1,4 +0,0 @@
-// Toggle functionality for the hamburger menu (just a placeholder functionality for now)
-document.getElementById('menuIcon').addEventListener('click', function() {
-    alert('Menu Clicked');
-});
